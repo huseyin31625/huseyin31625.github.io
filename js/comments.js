14edb916852f833c14b186bede25c5fa3023bb2a
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2021-09-09 16:39:43 +0300

    Update comments.js

--- js/comments.js
+++ js/comments.js
@@ -11,10 +11,16 @@ function GetURLParameter(sParam)
         }
     }
 }
+  if (GetURLParameter("id") == "g5Jkl") {// Save data to sessionStorage
+    let data = sessionStorage.getItem('commentg5Jkl');
+    document.getElementById("cmlist").innerHTML = data;
+  }
 // Get saved data from sessionStorage
 document.getElementById("writecomment").addEventListener("click", function() {
   if (GetURLParameter("id") == "g5Jkl") {// Save data to sessionStorage
+      var element = document.createElement("div");
+      element.className = "comment";
+      element.innerHTML = "<div class=\"comment-title\">" + document.getElementById("username").value + "</div>";
     sessionStorage.setItem('commentg5Jkl', document.getElementById("cmlist").innerHTML);
-    let data = sessionStorage.getItem('commentg5Jkl');
   }
 });
