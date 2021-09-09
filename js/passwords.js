8a2aab7ccab73ad93c367d847be4000b98dd04c5
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2021-09-09 18:07:00 +0300

    Update passwords.js

--- js/passwords.js
+++ js/passwords.js
@@ -2,5 +2,6 @@ document.getElementById("create").addEventListener("click", function() {
   var element = document.createElement("div");
   element.className = "password";
   element.innerHTML = document.getElementById("password").value;
-  document.body.appendChild(element);
+  document.getElementById("passwds").appendChild(element);
+  sessionStorage.setItem("passwords", element.innerHTML);
 });
