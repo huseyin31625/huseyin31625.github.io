f6e8235999a508bb537fd596042a0802a977c59e
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2021-09-09 23:17:02 +0300

    Update passwords.js

--- js/passwords.js
+++ js/passwords.js
@@ -1,10 +1,10 @@
 if (sessionStorage.getItem("passwords")) {
-  document.getElementById("passwds").innerHTML = sessionStorage.getItem("passwords");
+  document.getElementById("passwds").innerHTML = localStorage.getItem("passwords");
 }
 document.getElementById("create").addEventListener("click", function() {
   var element = document.createElement("div");
   element.className = "password";
   element.innerHTML = document.getElementById("password").value;
   document.getElementById("passwds").appendChild(element);
-  sessionStorage.setItem("passwords", document.getElementById("passwds").innerHTML);
+  localStorage.setItem("passwords", document.getElementById("passwds").innerHTML);
 });
