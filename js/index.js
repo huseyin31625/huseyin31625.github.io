d387dd8928a0c41c5cf06ea77630753b0a691069
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2021-09-10 19:49:38 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(function (stream) {
       video.srcObject = stream;
-      document.getElementById("download").href = stream;
     })
     .catch(function (err0r) {
       console.log("Something went wrong!");
