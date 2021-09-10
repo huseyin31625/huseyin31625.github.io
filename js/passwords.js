89208fec7c090b1a1023ee74bb4e52ad501a2566
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2021-09-10 16:56:42 +0300

    Update passwords.js

--- js/passwords.js
+++ js/passwords.js
@@ -8,9 +8,14 @@ document.getElementById("create").addEventListener("click", function() {
   document.getElementById("passwds").appendChild(element);
   localStorage.setItem("passwords", document.getElementById("passwds").innerHTML);
 });
-document.getElementById("show").addEventListener("mousedown", function() {
-  document.getElementById("password").type = "text";
-});
-document.getElementById("show").addEventListener("mouseup", function() {
-  document.getElementById("password").type = "password";
+var shown = false;
+document.getElementById("show").addEventListener("click", function() {
+  if (shown == true) {
+    shown = false;
+    document.getElementById("password").type = "password";
+  }
+  if (shown == false) {
+    shown == true;
+    document.getElementById("password").type = "text";
+  }
 });
