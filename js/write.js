7e047e66eb3e314b58287d5a953a20d36028d0e3
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2021-09-11 18:05:28 +0300

    Update write.js

--- js/write.js
+++ js/write.js
@@ -3,6 +3,11 @@ document.getElementById("bold").addEventListener("click", function() {
   var bold = "<b>" + selection + "</b>";
   document.getElementById("write-here").value = document.getElementById("write-here").value.replace(selection, bold);
 });
+document.getElementById("italic").addEventListener("click", function() {
+  var selection = document.getElementById("write-here").value.substring(document.getElementById("write-here").selectionStart, document.getElementById("write-here").selectionEnd);
+  var bold = "<i>" + selection + "</i>";
+  document.getElementById("write-here").value = document.getElementById("write-here").value.replace(selection, bold);
+});
 setInterval(function() {
   document.getElementById("preview").innerHTML = document.getElementById("write-here").value;
 }, 750);
