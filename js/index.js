242a25a3ca7c7ca83ed2741bc5c4d7d8ad574a40
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2021-09-11 13:29:05 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,1 @@
-var video = document.querySelector("#cameraElement");
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (err0r) {
-      console.log("Something went wrong!");
-    });
-}
