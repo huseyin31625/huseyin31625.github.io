b47dea415fc3a49450a1e56ffed2a0b780c6864b
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2021-09-11 14:58:37 +0300

    Create write.js

---
+++ js/write.js
@@ -0,0 +1,5 @@
+document.getElementById("bold").addEventListener("click", function() {
+  var selection = document.getElementById("write-here").value.substring(document.getElementById("write-here").selectionStart, document.getElementById("write-here").selectionEnd);
+  var bold = "<b>" + selection + "</b>";
+  document.getElementById("write-here").value = document.getElementById("write-here").value.replace(selection, bold);
+});
